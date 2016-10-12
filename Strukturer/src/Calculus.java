c770063fee70f10737c8ceac2dd4f63530edb6e5
Demo <jwow0501@gmail.com>
2016-10-12 10:33:55 +0200

    skapande av new prodject
    skapade new prodject och new classes

---
+++ Strukturer/src/Calculus.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Calculus {
+	public static void main(String[] args){
+	 
+	
+	
+	 
+	 Scanner noob = new Scanner(System.in);
+	 Scanner test = new Scanner(System.in);
+	 boolean KeepLooping = true;
+	 boolean ExitLoop;
+	 boolean N = KeepLooping;
+		
+	 
+	 while (KeepLooping){
+	 double ett, två;
+	 String svar;
+	 System.out.println("Enter first num: ");
+	 ett = noob.nextDouble();
+	 System.out.println("Enter second num: ");
+	 två = noob.nextDouble();
+	 System.out.println("Give me the operator");
+	 svar = noob.next();
+	 
+	
+	 
+	 
+	 switch (svar){
+	 case "+":
+		 System.out.println("Your result: "+(ett+två));
+		 break;
+	 case "-":
+		 System.out.println("Your result: "+(ett-två));
+		 break;
+	 case "*":
+		 System.out.println("Your result: "+(ett*två));
+		 break;
+	 case "/":
+		 System.out.println("Your result: "+(ett/två));
+		 if (två != 0){ System.out.println("Your result: "+(ett/två));
+		 } else { System.out.println("ERROR!!! Division with null ist not possble");
+		 }
+		 break;
+		
+	 
+	 }
+	 
+	 System.out.println("Is this your number? if Yes, please Press Y if Not please press N and try again");
+	
+
+	 String response = test.nextLine();
+	 if(response.equalsIgnoreCase("Y")) {
+		 KeepLooping = false;		 
+	 } 
+	
+	 }
+	 System.out.println("Program is terminated, BUT I'LL BE BACK!!");
+	 System.exit(0);
+	 }
+}
---
+++ Strukturer/src/GuessNumber.java
@@ -0,0 +1,14 @@
+import java.util;
+import java.util.Scanner;
+public class GuessNumber {
+	public static void main(String[] args){
+		// Number given randomly to you. 
+		// you guess the number
+		// if you close, very hot.
+		// if you far away, very cold.
+		// basic functions. 
+		
+		
+	}
+
+}
